7cbb47f2d535aaa0e94ab9d2ad6c12e361b4a84b
Евгений <Zhenya-Pankratov@yandex.ru>
2024-02-16 23:39:45 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import './App.css';
 
 function App() {
-  aaaa
   return (
     <>
-      <div>
-        Goood
-      </div>
+      <div>Goood</div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
